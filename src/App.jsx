83f20c6e6a46d7aa1545b4e0aeca82de65bb2bc2
Brian Pham <phambri97@gmail.com>
2023-08-09 22:24:11 -0700

    Adding extra time to avoid error page

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
   useEffect(() => {
     const loadElements = setTimeout(() => {
       setFirstRender(false);
-    }, 3500);
+    }, 3750);
 
     return () => clearTimeout(loadElements);
   }, []);
